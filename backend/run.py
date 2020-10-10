9b592911e086491077e852d662dd0da48d473efd
chenronghuai <337164910@qq.com>
2020-10-10 11:27:32 +0800

    1

--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,5 @@
 from driverreport import TestDriverReport
-import login
+from pathlib import Path
 import unittest
 import HTMLTestRunner
 from time import sleep, strftime
@@ -8,18 +8,15 @@ import os
 
 
 if __name__ == '__main__':
-    '''
-    for i in range(2):
-        driver = login.login('HTTP1', 'USER1')
-    #    utils.switch_frame(driver, '监控管理', '客户来电', 'customerCall.do')
-    #    utils.switch_frame(driver, '监控管理', '司机报班', 'driverReport.do')
-        sleep(2)
-        driver.quit()
-    '''
+
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(TestDriverReport))
     now_time = strftime("%Y-%m-%d %H-%M-%S")
-#    file_path = utils.get_path() + "//testreport//" + now_time + "_result.html"
+    report_path = Path(os.path.join(utils.get_path(), os.path.pardir) + '//testreport//')
+    if report_path.exists():
+        pass
+    else:
+        os.mkdir(os.path.join(utils.get_path(), os.path.pardir) + '//testreport//', 0o777)
     file_path = os.path.abspath(os.path.join(utils.get_path(), os.path.pardir)) + "//testreport//" + now_time + "_result.html"
     file_result = open(file_path, 'wb')
     runner = HTMLTestRunner.HTMLTestRunner(file_result, 2, u"业务后台测试报告", u"执行概况")
