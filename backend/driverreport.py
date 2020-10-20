9c879369b04ac53c0eff982d6357a2c4d39bcb87
chenronghuai <337164910@qq.com>
2020-10-20 11:09:05 +0800

    10.20日，取消报班时车牌与电话号码不匹配过滤修改，中途返回信息避免异常抛出。

--- backend/driverreport.py
+++ backend/driverreport.py
@@ -15,7 +15,7 @@ class TestDriverReport(unittest.TestCase):
     is_phone = True
     @classmethod
     def setUpClass(cls):
-        cls.driver = login.login('HTTP1', 'USER2')
+        cls.driver = login.login('HTTP2', 'USER1')
         utils.switch_frame(cls.driver, '监控管理', '司机报班', 'driverReport.do')
 
     @classmethod
@@ -62,6 +62,7 @@ class TestDriverReport(unittest.TestCase):
 
     def driver_report_by_carnum(self, carnum, phone):
         self.is_phone = False
+        self.b_driver = False
         e_carnum = self.driver.find_element_by_css_selector('#selCar')
         e_carnum.clear()
         self.driver.execute_script('$("table#data_table>tbody>tr").html("")')  # 清空表内容，避免用例交叉
@@ -75,14 +76,17 @@ class TestDriverReport(unittest.TestCase):
             )
         except:
             return '找不到该车牌关联的任何司机'
-#        sleep(1)  # 等待遮挡层消失，否则”报班“按钮接收不到点击事件？暂无更好办法
         records = self.driver.find_elements_by_css_selector('table#data_table>tbody>tr')
         for i in range(len(records)):
             css = '#data_table>tbody>tr:nth-child(%s)>td:nth-child(5)' % (i + 1)
             if self.driver.find_element_by_css_selector(css).text == phone:
+                self.b_driver = True
                 css_report = '#data_table>tbody>tr:nth-child(%s)>td:nth-child(15)>a[name="btnReport"]' % (i + 1)
                 self.driver.find_element_by_css_selector(css_report).click()
                 break
+        if not self.b_driver:
+            return '找不到该车牌和电话号码关联的司机'
+
         self.report_action()
         css_goal = 'tbody>tr:nth-child(%s)>td:nth-child(10)' % (i + 1)
         return self.driver.find_element_by_css_selector(css_goal).text
@@ -104,17 +108,19 @@ class TestDriverReport(unittest.TestCase):
         WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(
             (By.CSS_SELECTOR, 'div.layui-layer-btn.layui-layer-btn- > a.layui-layer-btn0'))).click()
         self.driver.switch_to.frame(self.driver.find_element_by_css_selector('iframe[src="/driverReport.do"]'))
-        self.driver.execute_script('$("table#data_table>tbody>tr").html("")')
 
         WebDriverWait(self.driver, 5).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, 'div[type="dialog"]')))
         WebDriverWait(self.driver, 5).until(EC.invisibility_of_element_located((By.CSS_SELECTOR, 'div[type="dialog"]')))
-
+#        WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.layui-layer-shade')))
         WebDriverWait(self.driver, 5).until(EC.invisibility_of_element_located((By.CSS_SELECTOR, '.layui-layer-shade')))
+        '''
+        self.driver.execute_script('$("table#data_table>tbody>tr").html("")')
         WebDriverWait(self.driver, 5).until(
             EC.visibility_of_element_located((By.CSS_SELECTOR, '#query_driver'))).click()
         WebDriverWait(self.driver, 5).until(
             EC.visibility_of_element_located((By.CSS_SELECTOR, 'table#data_table>tbody>tr>td:nth-child(10)')))
+        '''
         return self.driver.find_element_by_css_selector('tbody>tr>td:nth-child(10)').text
 
     @unittest.skip("直接跳过")
